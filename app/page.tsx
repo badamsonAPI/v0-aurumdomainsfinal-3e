6ffdad7fd8fd564cc2030df840a7736a6f0d5f47
badamsonAPI <bryanadamson88@gmail.com>
2025-04-25 15:48:11 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
             </header>
 
             <motion.div
-              className="text-center mb-20"
+              className="text-center mb-10"
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.8 }}
