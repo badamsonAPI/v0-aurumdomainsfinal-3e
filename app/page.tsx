2d9680f6854f82f00956a826092c6fbda0aaacd2
badamsonAPI <bryanadamson88@gmail.com>
2025-04-25 15:48:53 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
               transition={{ duration: 1.2, delay: 1 }}
             ></motion.div>
 
-            <div className="mb-20">
+            <div className="mb-10">
               <Accordion type="single" collapsible className="w-full">
                 <AccordionItem value="how-it-works" className="border-gold-900/30">
                   <AccordionTrigger className="text-[#D9C379] font-cormorant text-xl tracking-wider py-6 hover:text-gold-300 transition-colors">
